4792e2b1c8de3143a4444724716a0931db1d80a7
Isaac Culver <idculver27@gmail.com>
2024-06-18 23:27:18 -0500

    leaderboards work

--- src/index.js
+++ src/index.js
@@ -34,8 +34,8 @@ client.on("messageCreate", msg => {
 // commands
 client.on(Events.InteractionCreate, interaction => {
     if (!interaction.isChatInputCommand()) return;
-    if (interaction.commandName === "wordle") leaderboardWordle(interaction);
-    else if (interaction.commandName === "connections") leaderboardConnections(interaction);
+    if (interaction.commandName === "wordle") wordleLeaderboard(interaction);
+    else if (interaction.commandName === "connections") connectionsLeaderboard(interaction);
 });
 
 function saveWordleResults(msg) {
@@ -71,7 +71,7 @@ function saveConnectionsResults(msg) {
         let nickname = msg.member.displayName;
         let puzzleNum = msg.content.split("\n")[1].replace("Puzzle #", "");
         
-        let mistakes = 0;
+        let score = 0;
         let lines = msg.content.split("\n");
         for (let i = 2; i < lines.length; i++) {
             // if a line contains more than one color of square, it is a mistake
@@ -79,27 +79,86 @@ function saveConnectionsResults(msg) {
             let b = lines[i].includes("🟩");
             let c = lines[i].includes("🟦");
             let d = lines[i].includes("🟪");
-            if ((a ? 1 : 0) + (b ? 1 : 0) + (c ? 1 : 0) + (d ? 1 : 0) > 1) mistakes++;
+            if ((a ? 1 : 0) + (b ? 1 : 0) + (c ? 1 : 0) + (d ? 1 : 0) > 1) score++;
         }
         
         if (!(userId in results)) results[userId] = {};
         results[userId].nickname = nickname;
-        if (!("mistakes" in results[userId])) results[userId].mistakes = {};
-        results[userId].mistakes[puzzleNum] = mistakes;
+        if (!("scores" in results[userId])) results[userId].scores = {};
+        results[userId].scores[puzzleNum] = score;
 
         fs.writeFileSync(path, JSON.stringify(results, null, "\t"));
 
-        if (mistakes < 4) msg.react(happy);
-        else if (mistakes === 4) msg.react(sad);
+        if (score < 4) msg.react(happy);
+        else if (score === 4) msg.react(sad);
     } catch (error) {
         console.error(error);
     }
 }
 
-function leaderboardWordle(interaction) {
-    interaction.reply("I haven't implemented the Wordle leaderboard yet");
+function wordleLeaderboard(interaction) {
+    // tabulate results
+    const results = JSON.parse(fs.readFileSync("results/wordle.json"));
+    let entries = []
+    for (user in results) {
+        let scoreSum = 0;
+        let gamesPlayed = 0;
+        for (score in results[user].scores) {
+            if (results[user].scores[score] === "X") scoreSum += 7; // a loss is counted as 7 guesses
+            else scoreSum += parseInt(results[user].scores[score]);
+            gamesPlayed++;
+        }
+        let entry = {
+            nickname: results[user].nickname,
+            avgScore: Math.round(scoreSum / gamesPlayed * 100) / 100
+        };
+        entries.push(entry);
+    }
+    entries.sort((a, b) => a.avgScore - b.avgScore);
+
+    // find longest name
+    let longest = 0;
+    for (entry of entries) {
+        if (entry.nickname.length > longest) longest = entry.nickname.length;
+    }
+    // print leaderboard
+    let table = "Wordle Leaderboard (by average number of guesses)\n```\n";
+    for (let i = 0; i < entries.length; i++) {
+        table += "#" + (i + 1).toString().padEnd(4) + entries[i].nickname.padEnd(longest + 2) + entries[i].avgScore.toFixed(1) + "\n";
+    }
+    table += "```";
+    interaction.reply(table);
 }
 
-function leaderboardConnections(interaction) {
-    interaction.reply("I haven't implemented the Connections leaderboard yet");
+function connectionsLeaderboard(interaction) {
+    // tabulate results
+    const results = JSON.parse(fs.readFileSync("results/connections.json"));
+    let entries = []
+    for (user in results) {
+        let scoreSum = 0;
+        let gamesPlayed = 0;
+        for (score in results[user].scores) {
+            scoreSum += results[user].scores[score];
+            gamesPlayed++;
+        }
+        let entry = {
+            nickname: results[user].nickname,
+            avgScore: Math.round(scoreSum / gamesPlayed * 100) / 100
+        };
+        entries.push(entry);
+    }
+    entries.sort((a, b) => a.avgScore - b.avgScore);
+ 
+     // find longest name
+     let longest = 0;
+     for (entry of entries) {
+         if (entry.nickname.length > longest) longest = entry.nickname.length;
+     }
+     // print leaderboard
+     let table = "Connections Leaderboard (by average number of mistakes)\n```\n";
+     for (let i = 0; i < entries.length; i++) {
+         table += "#" + (i + 1).toString().padEnd(4) + entries[i].nickname.padEnd(longest + 2) + entries[i].avgScore.toFixed(1) + "\n";
+     }
+     table += "```";
+     interaction.reply(table);
 }
